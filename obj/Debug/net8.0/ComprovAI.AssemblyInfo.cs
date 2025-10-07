3bfc934955ef692cace34f5bc02e326f3bf113bd
ThiagoDev25 <thiagoafonsopaz@hotmail.com>
2025-10-07 10:21:10 -0300

    add_logo

--- obj/Debug/net8.0/ComprovAI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComprovAI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComprovAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac63566404e661c10e0da920b6215ff7160676d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8012a68704b2e42a4d1822b88bddb15187635049")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComprovAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComprovAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
